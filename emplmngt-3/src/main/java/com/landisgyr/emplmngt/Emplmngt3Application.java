45d82eb029186dabb361b8dfd9f74dd7b862f820
DhanetwS <DhanetwS@INDELNB654.ap.bm.net>
2021-08-01 15:03:56 +0530

    from local to central

---
+++ emplmngt-3/src/main/java/com/landisgyr/emplmngt/Emplmngt3Application.java
@@ -0,0 +1,13 @@
+package com.landisgyr.emplmngt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emplmngt3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Emplmngt3Application.class, args);
+	}
+
+}
---
+++ emplmngt-3/src/test/java/com/landisgyr/emplmngt/Emplmngt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landisgyr.emplmngt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Emplmngt3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
